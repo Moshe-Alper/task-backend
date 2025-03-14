12f1416d8f15939bcb5d0a4a6d44850f24cf9ede
Moshik Alper <moshikalper@gmail.com>
2025-03-14 15:17:18 +0200

    Init repo

---
+++ api/task/task.routes.js
@@ -0,0 +1,23 @@
+import express from 'express'
+
+import { requireAuth } from '../../middlewares/requireAuth.middleware.js'
+import { log } from '../../middlewares/logger.middleware.js'
+
+import { getTasks, getTaskById, addTask, updateTask, removeTask, addTaskMsg, removeTaskMsg } from './task.controller.js'
+
+const router = express.Router()
+
+// We can add a middleware for the entire router:
+// router.use(requireAuth)
+
+router.get('/', log, getTasks)
+router.get('/:id', log, getTaskById)
+router.post('/', log, requireAuth, addTask)
+router.put('/:id', requireAuth, updateTask)
+router.delete('/:id', requireAuth, removeTask)
+// router.delete('/:id', requireAuth, requireAdmin, removeTask)
+
+router.post('/:id/msg', requireAuth, addTaskMsg)
+router.delete('/:id/msg/:msgId', requireAuth, removeTaskMsg)
+
+export const taskRoutes = router
